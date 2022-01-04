d00b3f365ad76109712d3d3bc6423063b2b1165b
Claudia Souza <claudia.devsp@gmail.com>
2022-01-04 11:42:58 -0200

    feat: add next-auth with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,26 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss';
 
-const SignInButton = () => {
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ?
+const SignInButton = () => {    
+    const { data } = useSession();
+    return data ?
         (
             <button
                 type="button"
                 className={styles.signInButton}
+                onClick={() => {signOut()}}
             >
                 <FaGithub color="#04D361" />
-                Clau Souza
+                {data.user.name}
                 <FiX color="#737380" className={styles.closeIcon} />
             </button>
         ) : (
             <button
                 type="button"
                 className={styles.signInButton}
+                onClick={() => signIn('github')}
             >
                 <FaGithub color="#EBA417" />
                 Sign in with Github
