084af5380f04c50e53d6f8c2f89bd7fd3dcf3b27
Claudia Souza <claudia.devsp@gmail.com>
2022-01-04 16:42:59 -0200

    feat: add user in faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
 
 export default NextAuth({
   providers: [
@@ -13,4 +15,16 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        )
+      )
+      return true
+    },
+  }
 })
