d7a71c80ced575c0f84418cad4b59cc434d91d6f
Claudia Souza <claudia.devsp@gmail.com>
2022-01-06 16:27:49 -0200

    feat: listen events stripes/init posts in prisma

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import styles from './styles.modules.scss';
+
+const Posts = () => {
+    return (
+        <>
+        <Head>
+            <title>Posts Ignews</title>
+        </Head>
+        <main>
+            <div>
+                <a>
+                    <time></time>
+                </a>
+            </div>
+        </main>
+        </>
+    )
+}
+
+export default Posts;
